da6b63122c4462bc251f245b921e6ebcbd7afe9d
mazzillio <mateca500@gmail.com>
2023-05-11 19:09:19 -0300

    feat: created project

---
+++ src/main/java/com/mazzillio/DslistMazzillio/DslistMazzillioApplication.java
@@ -0,0 +1,13 @@
+package com.mazzillio.DslistMazzillio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistMazzillioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistMazzillioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mazzillio/DslistMazzillio/DslistMazzillioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazzillio.DslistMazzillio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistMazzillioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
