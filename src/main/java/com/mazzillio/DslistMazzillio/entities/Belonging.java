24dcbb2bd50cec3228ad00029d726a824674f3b0
mazzillio <mateca500@gmail.com>
2023-05-12 09:02:13 -0300

    feat: domain model

---
+++ src/main/java/com/mazzillio/DslistMazzillio/entities/Belonging.java
@@ -0,0 +1,51 @@
+package com.mazzillio.DslistMazzillio.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "belongings")
+public class Belonging {
+    @EmbeddedId
+    private BelongingPk id = new BelongingPk();
+    private Integer position;
+    public Belonging() {
+
+    }
+    public Belonging(Game game, GameList list, Integer position) {
+        this.id.setGame(game);
+        this.id.setList(list);
+        this.position = position;
+    }
+
+    public BelongingPk getId() {
+        return id;
+    }
+
+    public void setId(BelongingPk id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Belonging belonging)) return false;
+        return id.equals(belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/mazzillio/DslistMazzillio/entities/GameList.java
@@ -0,0 +1,48 @@
+package com.mazzillio.DslistMazzillio.entities;
+
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+@Entity
+@Table(name = "games_list")
+public class GameList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    public GameList() {}
+
+    public GameList(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object game) {
+        if (this == game) return true;
+        if (!(game instanceof GameList gameList)) return false;
+        return Objects.equals(getId(), gameList.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
