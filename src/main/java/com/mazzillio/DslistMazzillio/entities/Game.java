d7bbb0ef0462361cea878b61219b40768f4cd691
mazzillio <mateca500@gmail.com>
2023-05-11 22:07:15 -0300

    feat: game seeds

--- src/main/java/com/mazzillio/DslistMazzillio/entities/Game.java
+++ src/main/java/com/mazzillio/DslistMazzillio/entities/Game.java
@@ -4,41 +4,47 @@ import jakarta.persistence.*;
 
 import java.util.Objects;
 
-
 @Entity
 @Table(name = "games")
 public class Game {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(length = 30)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
     @Column(name = "game_year")
     private Integer year;
     private String genre;
+
+    private String platforms;
+
+    private Double score;
+
     private String imgUrl;
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
 
     }
 
-    public Game(String id, String title, Integer year, String genre, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String platforms, Double score,String genre, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
+        this.platforms = platforms;
+        this.score = score;
         this.genre = genre;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -64,6 +70,22 @@ public class Game {
 
     public void setGenre(String genre) {
         this.genre = genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
