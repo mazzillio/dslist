31d0de69913c6318e14987e92f08148f25a883a1
mazzillio <mateca500@gmail.com>
2023-05-14 10:34:43 -0300

    fix: gameRepository feat: configuration profiles

--- src/main/java/com/mazzillio/DslistMazzillio/repositotiries/GameRepository.java
+++ src/main/java/com/mazzillio/DslistMazzillio/repositotiries/GameRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
     @Query(nativeQuery = true, value = """
-		SELECT games.id, games.title, games.game_year AS `year`, games.img_url AS imgUrl,
+		SELECT games.id, games.title, games.game_year AS "year", games.img_url AS imgUrl,
 		games.short_description AS shortDescription, belongings.position
 		FROM games
 		INNER JOIN belongings ON games.id = belongings.game_id
